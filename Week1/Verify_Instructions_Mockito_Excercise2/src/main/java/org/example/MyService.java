0ae4875ffce3b6cfa0f190423a5f683a19145a17
audhee <audheeraj875@gmail.com>
2026-06-27 11:55:00 +0530

    Implemented the Instruction Verifying Excercise 2 in Mockito

---
+++ Week1/Verify_Instructions_Mockito_Excercise2/src/main/java/org/example/MyService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class MyService {
+    private ExternalApi api;
+    public MyService(ExternalApi api){
+        this.api = api;
+    }
+    public String fetchData(){
+        return api.getData();
+    }
+}
