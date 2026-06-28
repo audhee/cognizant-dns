d180ec18f5b9fe577a129d98d2ffead9365165eb
audhee <audheeraj875@gmail.com>
2026-06-28 16:26:27 +0530

    Implemented the Spring maven core project Library Management

---
+++ Week2/LibraryManagement/src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.Service.BookService;
+import org.example.repository.BookRepository;
+
+
+import org.example.repository.BookRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        service.displayService();
+        repository.displayRepository();
+    }
+}
---
+++ Week2/LibraryManagement/src/main/java/org/example/Service/BookService.java
@@ -0,0 +1,9 @@
+package org.example.Service;
+
+public class BookService {
+
+    public void displayService() {
+        System.out.println("Book Service Bean Created");
+    }
+
+}
